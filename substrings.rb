4c08c60ea0261bafbc330484b60a458a0cc053b1
Nick Martinovic <nmartinovictop@gmail.com>
2022-08-07 22:42:06 +0900

    update solution

--- substrings.rb
+++ substrings.rb
@@ -1,41 +1,36 @@
-# > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 # => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-# > substrings("below", dictionary)
-# => { "below" => 1, "low" => 1 }
-
-# > substrings("Howdy partner, sit down! How's it going?", dictionary)
-# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
 
 
+def substrings(words,dictionary)
+  hash_to_return = Hash.new(0)
 
-def substrings(words, dictionary)
-    return_hash = Hash.new(0)
+  words.split(" ").each do |word|
 
-
-    words.split(" ").each do |word|
-        word = word.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').downcase
-        (0...word.length).each do |n|
-            (0..n).each do |l|
-                if dictionary.include?(word[l..n].downcase)
-                    return_hash[word[l..n].downcase] += 1
-                end
-            end
-            
-
-    
+    (0...word.length).each do |i|
+      (0...word.length).each do |l|
+        if dictionary.include?(word[i..l].downcase)
+          hash_to_return[word[i..l].downcase] += 1
         end
-
+      end
     end
+  end
 
 
-
-
-
-    return_hash
+  hash_to_return
 end
 
 
-dictionary = ["below","down","go","going","horn","how",
-    "howdy","it","i","low","own","part","partner","sit"]
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+
+
+
+
+
+
+
+# substrings("below", dictionary)
 # => { "below" => 1, "low" => 1 }
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+  # => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
